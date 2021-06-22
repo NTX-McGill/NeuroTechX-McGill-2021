ba0f131ff3246db24367a3db6fac353a4e33ac5e
Ding Ma <ding.ma@mail.mcgill.ca>
2021-06-21 23:49:43 -0400

    freeze deps

--- software/data_collection_platform/backend/model/example.py
+++ software/data_collection_platform/backend/model/example.py
@@ -1,17 +1,49 @@
-import pyodbc
+
 from sqlalchemy import create_engine
-import urllib
-import os
+from sqlalchemy.orm import sessionmaker
 
-server = "tcp:mcgill-neurotech.database.windows.net,1433"
-db = "McGill_NeuroTech"
-usr = os.getenv("usr")
-pwd = os.getenv("pwd")
+from models import CollectedData, CollectionInstance, Video, Base
 
-params = urllib.parse.quote_plus(
-    fr'DRIVER={{ODBC Driver 17 for SQL Server}};Server={server};Database={db};Uid={usr};Pwd={pwd};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
-conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
-engine_azure = create_engine(conn_str, echo=True)
 
+# server = "tcp:mcgill-neurotech.database.windows.net,1433"
+# db = "McGill_NeuroTech"
+# usr = os.getenv("usr")
+# pwd = os.getenv("pwd")
+# params = urllib.parse.quote_plus(
+#     fr'DRIVER={{ODBC Driver 17 for SQL Server}};Server={server};Database={db};Uid={usr};Pwd={pwd};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+# conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
+# engine = create_engine(conn_str, echo=True)
+
+
+engine = create_engine('sqlite:///:memory:', echo=True)
+Base.metadata.bind = engine
+Base.metadata.create_all(engine)
 print('connection is ok')
-print(engine_azure.table_names())
+print(engine.table_names())
+DBSession = sessionmaker(bind=engine)
+
+session = DBSession()
+
+
+vid = Video(id="https://www.youtube.com/", start="1:10",
+            end="2:20", is_stressful=False)
+
+session.add(vid)
+session.commit()
+
+
+vids = [
+    Video(id="https://www.youtube.com/1", start="1:10",
+          end="2:20", is_stressful=False),
+    Video(id="https://www.youtube.com/2", start="1:10",
+          end="2:20", is_stressful=False),
+    Video(id="https://www.youtube.com/3", start="1:10",
+          end="2:20", is_stressful=False)
+]
+
+session.add_all(vids)
+
+
+in_db = session.query(Video).filter_by(id='https://www.youtube.com/1').first()
+
+print(in_db)
---
+++ software/data_collection_platform/backend/model/models.py
@@ -0,0 +1,69 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, Numeric, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+from datetime import datetime
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+import os
+import urllib
+import pyodbc
+
+Base = declarative_base()
+
+
+def auto_str(cls):
+    def __str__(self):
+        return '%s(%s)' % (
+            type(self).__name__,
+            ', '.join('%s=%s' % item for item in vars(self).items())
+        )
+    cls.__str__ = __str__
+    return cls
+
+
+@auto_str
+class CollectedData(Base):
+
+    __tablename__ = "collected_data"
+
+    id = Column(Integer, primary_key=True)
+    channel_1 = Column(Numeric)
+    channel_2 = Column(Numeric)
+    channel_3 = Column(Numeric)
+    channel_4 = Column(Numeric)
+    channel_5 = Column(Numeric)
+    channel_6 = Column(Numeric)
+    channel_7 = Column(Numeric)
+    channel_8 = Column(Numeric)
+    space_bar_status = Column(Boolean)
+    collection_instance = Column(String, ForeignKey("collection_instance.id"))
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+
+@auto_str
+class CollectionInstance(Base):
+    __tablename__ = "collection_instance"
+
+    id = Column(Integer, primary_key=True)
+    stress_level = Column(Integer)
+    video_id = Column(String, ForeignKey("video.id"))
+    date = Column(DateTime, default=datetime.utcnow)
+    video_id = Column(String, ForeignKey("video.id"))
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+
+@auto_str
+class Video(Base):
+    __tablename__ = "video"
+
+    id = Column(String, primary_key=True)
+    start = Column(String)
+    end = Column(String)
+    is_stressful = Column(Boolean)
+    keywords = Column(String)
+
+    def __repr__(self):
+        return str(self.__dict__)
