59807f197c09c3255b492a5204603d8a3d3dbdc3
Thomas Jiralerspong <thomasjiralerspong@gmail.com>
2021-06-23 13:04:09 -0400

    add get route for video database

--- software/data_collection_platform/backend/dcp/api.py
+++ software/data_collection_platform/backend/dcp/api.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request
 import dateutil.parser
+import pyodbc
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -20,3 +21,32 @@ def anxiety_down():
     time = dateutil.parser.isoparse(request.json['time'])
     print(f"Anxious stop at {time}")
     return {}, 200
+
+# DATABASE CONFIGURATION
+server = '<server>.database.windows.net'
+database = '<database>'
+username = '<username>'
+password = '<password>'   
+driver= '{ODBC Driver 17 for SQL Server}'
+table_name = ""
+
+@bp.route('/videos', methods=['GET'])
+def get_videos():
+    with pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+        with conn.cursor() as cursor:
+            vid_array = []
+
+            for row in cursor.execute("SELECT * FROM " + table_name):
+                new_entry = {}
+                new_entry["link"] = row.link
+                new_entry["start"] = row.start
+                new_entry["end"] = row.end
+                new_entry["is_stressful"] = row.is_stressful
+                new_entry["keywords"] = row.keywords
+                new_entry["video_id"] = row.link.split('/')[-1]
+                vid_array.append(new_entry)
+
+            return vid_array, 200
+
+            
+        
