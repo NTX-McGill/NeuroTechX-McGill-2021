c2c2db52cdd533ca16345dbb49b52d923282a04b
Er Jun Li <er.li@mail.mcgill.ca>
2021-07-03 03:19:10 -0400

    added not null fields to db

--- software/data_collection_platform/backend/dcp/models/data.py
+++ software/data_collection_platform/backend/dcp/models/data.py
@@ -8,18 +8,18 @@ class CollectedData(db.Model):
     __tablename__ = "collected_data"
 
     id = db.Column(db.Integer, primary_key=True)
-    channel_1 = db.Column(db.Float)
-    channel_2 = db.Column(db.Float)
-    channel_3 = db.Column(db.Float)
-    channel_4 = db.Column(db.Float)
-    channel_5 = db.Column(db.Float)
-    channel_6 = db.Column(db.Float)
-    channel_7 = db.Column(db.Float)
-    channel_8 = db.Column(db.Float)
-    is_subject_anxious = db.Column(db.Boolean)
+    channel_1 = db.Column(db.Float, nullable=False)
+    channel_2 = db.Column(db.Float, nullable=False)
+    channel_3 = db.Column(db.Float, nullable=False)
+    channel_4 = db.Column(db.Float, nullable=False)
+    channel_5 = db.Column(db.Float, nullable=False)
+    channel_6 = db.Column(db.Float, nullable=False)
+    channel_7 = db.Column(db.Float, nullable=False)
+    channel_8 = db.Column(db.Float, nullable=False)
+    is_subject_anxious = db.Column(db.Boolean, nullable=False)
     collection_instance_id = db.Column(
-        db.Integer, db.ForeignKey("collection_instance.id"))
-    order = db.Column(db.Integer)
+        db.Integer, db.ForeignKey("collection_instance.id"), nullable=False)
+    order = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return str(self.__dict__)
