f4b816ba6397e61d35125f4cf91971edbfc31aeb
Michael Li <er.li@mail.mcgill.ca>
2021-07-04 01:28:17 -0400

    Update software/data_collection_platform/backend/dcp/api.py
    Co-authored-by: Sheheryar Parvaz <me@cherryman.org>

--- software/data_collection_platform/backend/dcp/api.py
+++ software/data_collection_platform/backend/dcp/api.py
@@ -203,7 +203,7 @@ def get_task_result(task_id: str):
 
 @bp.route('/test', methods=['GET'])
 def test():
-    """Method to test Celery task queue setup
+    """Test Celery task queue setup.
     """
     import random
     a = random.randrange(1000)
