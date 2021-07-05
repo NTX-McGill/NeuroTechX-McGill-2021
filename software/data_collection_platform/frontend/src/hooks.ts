f4e0a1d7b8497f6ad16be03cb9c72bc35f40cfd7
Sheheryar Parvaz <me@cherryman.org>
2021-07-05 17:06:57 -0400

    Refactor frontend state and dcp code
    * Moved store to state
    * Moved all types together
    * Created async thunk for fetching videos

--- software/data_collection_platform/frontend/src/hooks.ts
+++ software/data_collection_platform/frontend/src/hooks.ts
@@ -3,7 +3,7 @@ import {
   useDispatch as useDispatch_,
   useSelector as useSelector_,
 } from "react-redux";
-import type { State, Dispatch } from "./store";
+import { State, Dispatch } from "./types";
 
 export const useDispatch = () => useDispatch_<Dispatch>();
 export const useSelector: TypedUseSelectorHook<State> = useSelector_;
