57387795aa1ca0e767c18c15811fc2c2e0c50f4f
Sheheryar Parvaz <me@cherryman.org>
2021-07-05 22:40:48 -0400

    Remove styled-components

--- software/data_collection_platform/frontend/src/components/VideoSelectionPage/index.tsx
+++ software/data_collection_platform/frontend/src/components/VideoSelectionPage/index.tsx
@@ -3,21 +3,22 @@ import { useSelector, useDispatch } from "../../hooks";
 import { videos as videosActions } from "../../state";
 import { VideoInfo } from "../../types";
 import Thumbnail from "./Thumbnail";
-import {
-  VideoSelectionContainer,
-  SelectionPageContainer,
-} from "./__styled__/VideoSelectionPage";
+import "./index.scss";
 
 const VideoSelectionPage = () => {
   const dispatch = useDispatch();
   const { videos, videosToWatch } = useSelector((state) => state.videos);
+
+  const checked = videosToWatch.reduce(
+    (acc, v) => acc.add(v.id),
+    new Set<number>()
+  );
 
   useEffect(() => {
     if (!videos.length) dispatch(videosActions.fetch());
   }, [dispatch, videos.length]);
 
   const handleOnCheckChange = (video: VideoInfo, checked: boolean) => {
-    // add video to the list of videos in the state if checked else remove the video from the list
     const newVideosToWatch = checked
       ? [...videosToWatch, { ...video }]
       : videosToWatch.filter((vid) => vid.id !== video.id);
@@ -26,18 +27,19 @@ const VideoSelectionPage = () => {
   };
 
   return (
-    <SelectionPageContainer>
+    <div className="VideoSelectionPage">
       <h1>Select At Least 1 Video To Watch</h1>
-      <VideoSelectionContainer>
+      <div className="VideoSelectionContainer">
         {videos.map((video) => (
           <Thumbnail
             key={video.id}
             video={video}
+            checked={checked.has(video.id)}
             onCheckChange={handleOnCheckChange}
           />
         ))}
-      </VideoSelectionContainer>
-    </SelectionPageContainer>
+      </div>
+    </div>
   );
 };
 
