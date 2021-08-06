363ea466a9df52f371b2f3087b7a71a2b8902b61
Rebecca <rebecca.salganik@gmail.com>
2021-08-06 11:52:42 -0400

    LT2 vs ltsamp

--- ml/wqrs.py
+++ ml/wqrs.py
@@ -1,67 +1,100 @@
+from scipy import signal
 from hrv_extraction import Spider_Data_Loader 
 from utils import load_visualise, upsample
 import heartpy as hp 
 import matplotlib.pyplot as plt 
-import numpy as np 
+import numpy as np
+from math import ceil, sqrt
 
-def upsample(ecg): 
+'''
+OPEN ISSUES: 
+- How to decide on BUFLN?  
+
+'''
+
+
+def upsample(ecg, sr, fs=250): 
     data_length = int(ecg.shape[0] / sr)
     og_sample_pts = np.arange(0, data_length, 1/sr)
-    new_sample_pts = np.arange(0, data_length, 1/FS)
+    new_sample_pts = np.arange(0, data_length, 1/fs)
     return np.interp(new_sample_pts, og_sample_pts, ecg[:og_sample_pts.shape[0]])
 
 def sec_to_pts(s, fs=250):
     return int(s * fs)
 
-def ltsamp(sig, t, LPn, LP2n, lfsc, LTwindow):  
+def ltsamp(sig, t):  
     Yn = Yn1 = Yn2 = 0 
-    print(int(LTwindow))
-    ebuf, lbuf = np.array([np.sqrt(lfsc) for i in range(int(LTwindow))]), np.zeros(int(LTwindow)) 
+    ebuf, lbuf = np.array([np.sqrt(lfsc) for i in range(8192)]), [] # np.zeros(8192) 
     tt =  -1 
     aet = 0
-    
     while(t > tt): 
-        # print("tt: {}, t: {}".format(tt, t))
-        # print("ebuf: {}, lbuf: {}".format(ebuf, lbuf))
-        # print("tt: {}, tt-LPn: {}, LP2n:{} ".format(tt, tt-LPn, tt-LP2n))
-        v0, v1, v2 = sig[tt], sig[int(tt-LPn)], sig[int(tt-LP2n)]
+        Yn2 = Yn1 
+        Yn1 = Yn
+        v0, v1, v2 = sig[tt], sig[tt-LPn], sig[tt-LP2n]
         Yn = 2*Yn1 - Yn2 + v0 - 2*v1 + v2
         dy = (Yn-Yn1)/LP2n #Lowpass derivative of input 
         tt += 1
         et =  np.sqrt(lfsc + dy * dy)
-        # print("VALUE1: {} of ebuf".format(tt))
         ebuf[tt] = et 
-        aet += et - ebuf[int(tt-LTwindow)]
-        # print("VALUE2: {} of lbuf".format(tt-LTWindow))
-        lbuf[tt] = aet 
-        print("ebuf: {}, lbuf: {}".format(ebuf, lbuf))
-    return lbuf[-1]
+        # ebuf.append(et)
+        aet += et - ebuf[tt-LTwindow]
+        # lbuf[tt] = aet 
+        lbuf.append(aet)
+    return lbuf[t]
 
-def detect(sig, sr): 
-    print("***Setting Global Vars***")
-    sps = sr
+def LT2(sig, t, C = 1): 
+    y = sig[t-LTwindow: t]
+    delta_y = y[1:] - y[:-1]
+    return np.sum(np.sqrt(C + delta_y**2))
+
+def LT(x, C=100, w=130):
+    def _LTi(x, i, C, w_len):
+        y = x[i-w_len: i]
+        delta_y = y[1:] - y[:-1] / LP2n #added this scaling constant 
+        return np.sum(np.sqrt(C + delta_y**2))
+
+    w_len = sec_to_pts(w, fs=FS)
+    _LTi_vect = np.vectorize(lambda i: _LTi(x, i, C, w_len))
+    return _LTi_vect(np.arange(w_len, x.shape[0]))
+
+
+def detect(sig): 
     FROM = 0 
     to = len(sig)
+    timer = 0 
     next_minute = FROM + spm 
+    peaks = [] 
     
-    v = ltsamp(sig, 0, LPn, LP2n, lfsc, LTwindow)
-    print(v)
-    exit() 
+    print("***Calculating Initial Threshold***")
+    t1 = FROM + sec_to_pts(8)
 
-    print("***Calculating Average***")
-    t1 = FROM + sec_to_pts(8)   
-    T0 = 0 
+    #My Init Threshold    
+    # T0 = 0  
+    # for t in range(FROM, t1): 
+    #     T0 += ltsamp(sig, t)
+    # T0 /= t1 - FROM 
+    # Ta = 3 * T0 
+
+    LT_vals = [] 
     for t in range(FROM, t1): 
-        print("LOADING INFO FOR SAMPLE: {}".format(t))
-        T0 += ltsamp(sig, t, LPn, LP2n, lfsc, LTwindow)
-    T0 /= t1 - FROM 
-    Ta = 3 * T0 
-
+        LT_vals.append(LT2(sig, t))
+    T0 = np.mean(LT_vals)
+    Ta = T0 * 3
+    
+    #ROLAND's Init threshold 
+    # LT_ecg = LT(sig, C=1, w=.13)
+    # T02= np.mean(LT_ecg[FROM:t1]) 
+    # Ta2 = T02 * 3
+    
+    # print("compare thresholds: mine: {}, R: {}".format(Ta, Ta2))
+    # load_visualise(sig[FROM:t1], [Ta2 for i in range(t1-FROM)]) 
      
+    print("***Initial Threshold Set to: {}".format(Ta)) 
 
     # Main loop 
     for t in range(FROM, to): 
-        print('***Running Values for Time: {}'.format(t))
+        # if t % 250  == 0:  
+        #     print('***Running Values for: {} sec / {}'.format(t/250, int(to-FROM)/250))
         learning = 1
         if learning: 
             if t > t1: 
@@ -71,26 +104,29 @@ def detect(sig, sr):
             else: 
                 T1 = 2*T0 
 	    #Compare a length-transformed sample against T1.
-        if (ltsamp(sig, t, LPn, LP2n, lfsc, LTwindow) > T1): #Found possible QRS near t
+        if (LT2(sig, t) > T1): #Found possible QRS near t
+            print("***Potential QRS Found***")
             timer = 0 #Used for counting time after previous QRS 
-            max = min = ltsamp(sig, t, LPn, LP2n, lfsc, LTwindow)
+            max = min = LT2(sig, t) #ltsamp(sig, t)
+            print("***Finding Max***")
             for tt in range(t+1,t + EyeClosing/2): 
-                if (ltsamp(sig, tt, LPn, LP2n, lfsc, LTwindow) > max): max = ltsamp(tt)
+                if LT2(sig, tt > max): max = LT2(sig, tt) #(ltsamp(sig, tt) > max): max = ltsamp(sig, tt)
+            print("***Finding Min***")
             for tt in range(t-1, t - EyeClosing/2, -1): 
-                if (ltsamp(sig, tt, LPn, LP2n, lfsc, LTwindow) < min): min = ltsamp(tt)
+                if LT2(sig, tt < min): min = LT2(sig, tt) #(ltsamp(sig, tt) < min): min = ltsamp(sig, tt)
             if (max > min+10): #There is a QRS near tt
                 #Find QRS onset (PQ Junction)
                 onset = max/100 + 2 
                 tpq = t - 5 
                 for tt in range(t, t-EyeClosing/2, -1): 
-                    if (ltsamp(sig, tt, LPn, LP2n, lfsc, LTwindow) - ltsamp(sig, tt-1, LPn, LP2n, lfsc, LTwindow) < onset and 
-                        ltsamp(sig, tt-1, LPn, LP2n, lfsc, LTwindow) - ltsamp(sig, tt-2, LPn, LP2n, lfsc, LTwindow) < onset and 
-                        ltsamp(sig, tt-2, LPn, LP2n, lfsc, LTwindow) - ltsamp(sig, tt-3, LPn, LP2n, lfsc, LTwindow) < onset and 
-                        ltsamp(sig, tt-3, LPn, LP2n, lfsc, LTwindow) - ltsamp(sig, tt-4, LPn, LP2n, lfsc, LTwindow) < onset): 
+                    if (LT2(sig, tt) - LT2(sig, tt-1) < onset and 
+                        LT2(sig, tt-1) - LT2(sig, tt-2) < onset and 
+                        LT2(sig, tt-2) - LT2(sig, tt-3) < onset and 
+                        LT2(sig, tt-3) - LT2(sig, tt-4) < onset): 
                         tpq = tt - LP2n # account for phase shift 
                         break 
                 if (not learning): 
-                    peaks = tpq
+                    peaks.append(tpq) 
                 #Adjust Thresholds 
                 Ta += (max - Ta)/10 
                 T1 = Ta/3 
@@ -106,38 +142,34 @@ def detect(sig, sr):
         if (t >= next_minute): 
             next_minute += spm 
             print(".") 
-
-
-#Define Global Variables 
-FS = 250 #Sampling Frequency 
-BUFLN = 16384 #must be a power of 2, see ltsamp() 
-EYE_CLS = 0.25 #eye-closing period is set to 0.25 sec (250 ms)
-MaxQRSw = 0.13 #maximum QRS width (130ms)
-NDP	 = 2.5 #adjust threshold if no QRS found in NDP seconds 
-PWFreqDEF = 60 #power line (mains) frequency, in Hz (default)
-TmDEF	=  100 #minimum threshold value (default)
-
+    load_visualise(sig, peaks)
 
 print("***Loading Data***")
 spider = Spider_Data_Loader()
 ecg, gsr, sr = spider.load_physiodata('drive01')
-# print("***Loaded Data***")
 
 print("***Upsampling Data***")
-upsampled_ecg = upsample(ecg)
+upsampled_ecg = upsample(ecg, sr, fs=250)
 
-sps = sr
-samplingInterval = 1000.0/sps  
+print("***Init for Global Vars***")
+FS = sps = 250 #Sampling Frequency 
+# BUFLN = 16384 #must be a power of 2, see ltsamp() 
+EYE_CLS = 0.25 #eye-closing period is set to 0.25 sec (250 ms)
+MaxQRSw = 0.13 #maximum QRS width 0.13 sec (130ms)
+NDP	 = 2.5 #adjust threshold if no QRS found in NDP seconds 
+PWFreqDEF = 60 #power line (mains) frequency, in Hz (default)
+TmDEF	=  100 #minimum threshold value (default)
+# samplingInterval = 1000.0/sps  
 lfsc = 1 # FIX THIS EVENTUALLY TO BE : 1.25*gain*gain/sps
-spm = 60 * sps
-LPn = sps/PWFreqDEF; 	#The LP filter will have a notch at the power line (mains) frequency 
+spm = 60 * sps #samples per minute 
+LPn = int(sps/PWFreqDEF); 	#The LP filter will have a notch at the power line (mains) frequency 
 if (LPn > 8):  LPn = 8	# avoid filtering too agressively 
-LP2n = 2 * LPn
-EyeClosing = sps * EYE_CLS # set eye-closing period */
-ExpectPeriod = sps * NDP	#maximum expected RR interval */
-LTwindow = sps * MaxQRSw  #length transform window size */
+LP2n = int(2 * LPn)
+EyeClosing = int(sps * EYE_CLS) # set eye-closing period (in samples)
+ExpectPeriod = int(sps * NDP)	#maximum expected RR interval (in samples)
+LTwindow = int(sps * MaxQRSw)  #length transform window size (in samples)
 
-detect(upsampled_ecg, 250)
+detect(upsampled_ecg)
 
 # /******************************************************************************
 
@@ -193,4 +225,4 @@ detect(upsampled_ecg, 250)
 #     ltsample(3L);
 
 #     return 0;
-#}
+# }
