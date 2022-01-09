4898fc3373fc3722882a5e65dacc66a26730f037
Er Jun Li <er.li@mail.mcgill.ca>
2022-01-09 12:01:22 -0500

    Removing celery related boilerplate code.

--- software/speller/data_collection_platform/dcp/__init__.py
+++ software/speller/data_collection_platform/dcp/__init__.py
@@ -10,14 +10,10 @@ from dcp.utils.celery import init_celery
 
 from dcp.cfg.config import app_configs
 
-# TODO ALex: DB setup to be added later
 # FLASK EXTENSIONS
 # global database object
 db = SQLAlchemy()
 migrate = Migrate(compare_type=True)
-# celery = Celery(__name__, broker=app_configs.broker_url,
-#                 backend=app_configs.result_backend,
-#                 include=["dcp.tasks"])
 cors = CORS()
 
 
@@ -26,19 +22,14 @@ def create_app():
 
     app.config.from_object(app_configs)
 
-    # celery.conf.update(app.config)
-    # # configure celery tasks to run within app context
-    # init_celery(app=app, celery=celery)
-
     # create logs directory if not exists
     os.makedirs(os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "logs"), exist_ok=True)
 
     with app.app_context():
-        # from dcp import api
-        # app.register_blueprint(api.bp)
+        from dcp import api
+        app.register_blueprint(api.bp)
 
-        # # importing models so that Flask-Migrate can detect them
-        # # TODO Alex import the correct database models
+        # importing models so that Flask-Migrate can detect them
         from dcp.models.collection import CollectionInstance
         from dcp.models.configurations import OpenBCIConfig
         from dcp.models.data import CollectedData
@@ -46,7 +37,7 @@ def create_app():
         # # initialize extensions
         cors.init_app(app)
         db.init_app(app)
-        db.create_all()  # NOTE: creates all db tables (will not recreate tables that already exist)
+        db.create_all()  # creates all db tables (NOTE: will not recreate tables that already exist)
         migrate.init_app(app, db)
 
     return app
