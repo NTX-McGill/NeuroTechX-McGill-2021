9b8d8df8c50014ad6b791c6f8890b52766b377e2
Rebecca Salganik <39888337+Rsalganik1123@users.noreply.github.com>
2022-01-12 20:50:51 -0500

    Add files via upload

---
+++ Siggy_New/CCA_analysis_matlab/standardCCA_ITCCA_modified.m
@@ -0,0 +1,21 @@
+function r = standardCCA_ITCCA_modified(Signal, fundFrequency, numHarmonics, Template)
+[r1, Wxy, ~, ~] = standardCCA(Signal, fundFrequency, numHarmonics);
+[r5, Wx, Wxx] = IT_CCA(Signal, Template);
+r2 = corrcoef([Signal*(Wx(:,1)) Template*(Wx(:,1))]);
+r3 = corrcoef([Signal*(Wxy(:,1)) Template*(Wxy(:,1))]);
+[~, Wxxy, ~, ~] = standardCCA(Template, fundFrequency, numHarmonics);
+r4 = corrcoef([Signal*(Wxxy(:,1)) Template*(Wxxy(:,1))]);
+rho_array = zeros(1,8);
+rho_array(1)=r1(1);
+rho_array(2)=r2(1,2);
+rho_array(3)=r3(1,2);
+rho_array(4)=r4(1,2);
+rho_array(5)=r5(1);
+r6 = corrcoef([Signal*(Wxx(:,1)) Template*(Wxx(:,1))]);
+r7 = corrcoef([Signal*(Wx(:,1)) Signal*(Wxx(:,1))]);
+r8 = corrcoef([Signal*(Wxx(:,1)) Template*(Wx(:,1))]);
+rho_array(6)=r6(1,2);
+rho_array(7)=r7(1,2);
+rho_array(8)=r8(1,2);
+r=sum(sign(rho_array).*power(rho_array,2));
+end
