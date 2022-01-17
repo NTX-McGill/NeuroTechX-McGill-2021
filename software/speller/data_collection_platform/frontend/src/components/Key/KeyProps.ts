080ce87e86d50fec1451a27a13f4317c986070ae
Stephen Lu <stephen@heroai.ca>
2022-01-17 11:39:31 -0500

    Add and run prettier

--- software/speller/data_collection_platform/frontend/src/components/Key/KeyProps.ts
+++ software/speller/data_collection_platform/frontend/src/components/Key/KeyProps.ts
@@ -1,10 +1,10 @@
-interface KeyProps{
-    dispChar: string | JSX.Element;
-    outputChar: string;
-    phase: number;
-    freq: number;
-    color: string;
-    width: string;
+interface KeyProps {
+  dispChar: string | JSX.Element;
+  outputChar: string;
+  phase: number;
+  freq: number;
+  color: string;
+  width: string;
 }
 
 export default KeyProps;
--- software/speller/data_collection_platform/frontend/src/components/Keyboard/KeyboardState.ts
+++ software/speller/data_collection_platform/frontend/src/components/Keyboard/KeyboardState.ts
@@ -1,9 +1,9 @@
-import KeyMap from "./KeyMap";
+import KeyMap from './KeyMap';
 
-interface KeyboardState{
-    keys: KeyMap;
-    running: boolean;
-    [key: string]: any;
+interface KeyboardState {
+  keys: KeyMap;
+  running: boolean;
+  [key: string]: any;
 }
 
 export default KeyboardState;
