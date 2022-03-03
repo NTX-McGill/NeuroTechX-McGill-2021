5463e0152dc350f3b1e21c8d6fa6bb4753947528
Parth Gupta <69029756+gupta-parth@users.noreply.github.com>
2022-03-03 04:37:20 +0000

    Clear Database Feature (#105)
    * Added initial file
    
    * Cleared rows of collected_data database
    
    * Added a command to clear the database and changed __init__.py so that it works
    
    * Added a comment for the blueprint
    
    * Restructured
    
    * Changed command name to cleardb

---
+++ software/speller/data_collection_platform/backend/dcp/clear_database.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from flask import Blueprint
+
+from dcp import db
+from dcp.models.data import CollectedData
+from dcp.models.collection import BCICollection
+
+
+usersbp = Blueprint('users', __name__)
+
+
+@usersbp.cli.command('cleardb')
+def cleardb():
+    db.session.query(CollectedData).delete()
+    db.session.query(BCICollection).delete()
+    db.session.commit()
+    print("Deleted data")
