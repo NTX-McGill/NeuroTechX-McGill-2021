3311a207d193178f49f6bf2c7c07dce5a382f931
Pingsheng-Kevin <pingsheng.li@mail.mcgill.ca>
2022-05-09 21:38:54 -0400

    functions and template updated

--- software/speller/data_collection_platform/backend/dcp/signals/predict.py
+++ software/speller/data_collection_platform/backend/dcp/signals/predict.py
@@ -12,6 +12,7 @@ def predict_letter(bci_data, subject_id='S08'):
     low_bound_freq = 5.5
     upper_bound_freq = 54.0
     num_harmonics = 5  # parameter of FBCCA
+    onset = 35  # remove visual latency
     # dummy = np.random.rand(500, 8)  # 8 channels of 2s data, sampling rate = 250Hz
     # Template for each subject is stored locally, will be loaded as matlab array (for convenience) before inference.
     # Could be implemented differently
@@ -34,15 +35,14 @@ def predict_letter(bci_data, subject_id='S08'):
     with open("freq_letter_map.json") as fp:
         freq_letter_dict = json.load(fp)
     # print(freq_letter_dict)
-
+    signal_len = np.shape(bci_data)[0]
     for frequency in list(freq_letter_dict.keys()):  # Do FBCCA for every frequency, find the one with max corr
 
-        rho = filter_bank_cca_it(bci_data, float(frequency), low_bound_freq, upper_bound_freq, num_harmonics,
-                                 template.get(float(frequency)).astype(float), sampling_rate)
+        rho = filter_bank_cca_it(bci_data[onset:, :], float(frequency), low_bound_freq, upper_bound_freq, num_harmonics,
+                                 template.get(float(frequency)).astype(float)[onset:signal_len+1, :], sampling_rate)
         corr.append(rho)
     prediction_index = np.argmax(corr)
     predicted_letter = freq_letter_dict.get(list(freq_letter_dict.keys())[prediction_index])
-    print(predicted_letter)
 
     return predicted_letter
 
